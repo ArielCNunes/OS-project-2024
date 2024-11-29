1f58f71470b05f80756a04340d16a765c7ddf6e4
Ariel <arielc.nunes@outlook.com>
2024-11-29 10:27:26 +0000

    Added 'ConcurrentHashMap<String, User> users' to store all users and also added a method to register new users and the constructor which initializes our hash map

--- src/UserManager.java
+++ src/UserManager.java
@@ -1,4 +1,12 @@
+import java.util.concurrent.ConcurrentHashMap;
+
 public class UserManager {
+    // Store all users
+    private final ConcurrentHashMap<String, User> users;
+
+    public UserManager() {
+        users = new ConcurrentHashMap<>();
+    }
     // This method authenticates log information
     public boolean authenticate(String username, String password) {
         return true;
@@ -12,5 +20,14 @@ public class UserManager {
     // This method changes the password
     public boolean updatePassword(String email, String newPassword) {
         return false;
+    }
+
+    // This email registers the user (if the user is not already registered)
+    public boolean registerNewUser(String username, String password, String email, String department, String role, int employeeID) {
+        if (users.containsKey(email)) {
+            return false; // Email already registered
+        }
+        users.put(email, new User(username, password, email, department, role, employeeID)); // Add new user
+        return true;
     }
 }
